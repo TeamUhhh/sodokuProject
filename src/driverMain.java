e103347c03ecdb4b31698f4f18a2bb7315af6ec3
TestUser <smeriwether714@gmail.com>
2013-04-11 00:20:27 -0500

    startover

--- src/driverMain.java
+++
@@ -1,9 +0,0 @@
-
-public class driverMain {
-	public static void main(String[] args){
-		// makes a new Start Window
-		StartMenu frameOne = new StartMenu(false);
-		frameOne.setVisible(true);
-	}
-
-}
